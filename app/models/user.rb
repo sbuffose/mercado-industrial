e2e55ae3159f3a21495c071ea181708dc516bcc1
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-03-28 18:22:27 -0300

    add User model fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
