2f45cc03b84f42f115728bed4a944755c1e55d44
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-03-29 17:00:20 -0300

    upgraded yarn && add test for Bidding model

--- test/models/bidding_test.rb
+++ test/models/bidding_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class BiddingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "returns invalid if bidder or product is not supplied" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
 end
