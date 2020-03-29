452c40195728393d0cac66076422190414a8e0ec
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-03-29 00:13:51 -0300

    add #full_name for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   validates :phone_number, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}".strip
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,9 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(first_name: "john", last_name: "lennon")
     assert_equal "John Lennon", user.full_name
   end
+
+  test "full_name returns email when not first_name and last_name not provided" do
+    user = User.new(email: "john.lennon@gmail.com")
+    assert_equal "john.lennon@gmail.com", user.full_name
+  end
 end
