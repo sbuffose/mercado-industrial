9a4fe85058f16d236a1104d13ccfdbbce9f8e666
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-03-29 17:03:49 -0300

    fix on fixtures load for Bidding test

--- test/models/bidding_test.rb
+++ test/models/bidding_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class BiddingTest < ActiveSupport::TestCase
   test "returns invalid if bidder or product is not supplied" do
-    no_product = Bid.new(bidder: user(:seb))
-    no_bidder = Bid.new(product: product(:new_inyector))
-    bid = Bid.new(bidder: user(:seb), product: product(:new_inyector))
+    no_product = Bidding.new(bidder: users(:seb))
+    no_bidder = Bidding.new(product: products(:new_inyector))
+    bid = Bidding.new(bidder: users(:seb), product: products(:new_inyector))
 
     assert_equal no_product.valid?, false
     assert_equal no_bidder.valid?, false
