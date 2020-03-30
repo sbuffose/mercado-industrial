483f2f2e97f1ce96e57054b38b2f04f67712dff1
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2020-03-30 00:26:37 -0300

    remove system test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,10 +1,5 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  test "visiting the landing page" do
-    visit "/"
 
-    # save_and_open_screenshot
-    assert_selector "h1", text: "Awesome Products"
-  end
 end
